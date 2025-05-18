c401119179ff511fa814ea3d2e879a3214224b84
Bazhenov Yan <bazhenovyan@yandex.ru>
2025-05-18 20:23:40 +0400

    Added homework_7

---
+++ src/ClassWithMethods.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class ClassWithMethods {
+    Scanner scanner = new Scanner(System.in);
+    int a = 10;
+    int b = scanner.nextInt();
+    public void except() { // один catch
+        try {
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            if (b == 0) {
+                System.out.println("На ноль делить нельзя");
+            }
+        }
+        scanner.close();
+    }
+    public void moreExcept() { // несколько catch
+        try {
+            int[] c = new int[5];
+            c[5] = b;
+        } catch (ArithmeticException e) {
+            if (b == 0) {
+                System.out.println("На ноль делить нельзя");
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Выход за пределы массива");
+        }
+    }
+        public void multiExcept () { // несколько catch
+            try {
+                System.out.println(a / b);
+            } catch (NullPointerException | ArithmeticException e) {
+                System.out.println("Неверное значение для операции");
+            }
+        }
+            public void finallyExcept () { // несколько catch
+                try {
+                    System.out.println(a / b);
+                } catch (ArithmeticException e) {
+                    if (b == 0) {
+                        System.out.println("На ноль делить нельзя");
+                    }
+                }
+                finally {
+                    System.out.println("Завершение программы");
+                }
+        }
+    }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*Создать класс с как минимум 4 разными методами, в каждом методе добавить
+проверку на исключительные ситуации, используя:
+        1. try- catch
+        2. try-catch, где catch будет несколько
+3. try -catch, с использованием multi - catch
+        4. try-catch-finally
+Продемонстрировать обработку исключительных ситуаций на примерах*/
 public class Main {
     public static void main(String[] args) {
-
-
-
+    ClassWithMethods cl = new ClassWithMethods();
+    cl.except();
+    cl.moreExcept();
+    cl.multiExcept();
+    cl.finallyExcept();
     }
 }
